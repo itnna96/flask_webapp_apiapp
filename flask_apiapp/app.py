c1981e66c911faa1bab033d5dc15e1b74caba99e
Nam G VU <namgivu@gmail.com>
2022-09-04 16:57:27 +0700

    mock into region

--- flask_apiapp/app.py
+++ flask_apiapp/app.py
@@ -1,11 +1,6 @@
 from flask import Flask, jsonify
 
 
-mock_course_l = [
-    {'name': 'Docker Container',  'fee': '2,500,000₫', 'desc': 'Khóa học trang bị nền tảng kiến thức về container và triển khai ứng dụng theo kiến trúc microservice', },
-    {'name': 'Python for DevOps', 'fee': '5,500,000₫', 'desc': 'Tổng quan: Python hiện đang là 1 trong những ngôn ngữ lập trình phổ biến nhất cho người mới bắt đầu với CNTT nhờ…', },
-]
-
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -15,11 +10,27 @@ def index():
 
 #region /tel4vn_course
 def GET_tel4vn_course():
-    return jsonify(mock_course_l)
+    return GET_mock_tel4vn_course()  #TODO replaced this by real db data
 
 @app.route('/tel4vn_course', methods=['GET'])
 def tel4vn_course():
     return GET_tel4vn_course()
+
+
+#region mock
+@app.route('/mock/tel4vn_course', methods=['GET'])
+def mock_tel4vn_course():
+    return GET_tel4vn_course()
+
+mock_course_l = [
+    {'name': 'Docker Container',  'fee': '2,500,000₫', 'desc': 'Khóa học trang bị nền tảng kiến thức về container và triển khai ứng dụng theo kiến trúc microservice', },
+    {'name': 'Python for DevOps', 'fee': '5,500,000₫', 'desc': 'Tổng quan: Python hiện đang là 1 trong những ngôn ngữ lập trình phổ biến nhất cho người mới bắt đầu với CNTT nhờ…', },
+]
+
+def GET_mock_tel4vn_course():
+    return jsonify(mock_course_l)
+#endregion mock
+
 #endregion /tel4vn_course
 
 
