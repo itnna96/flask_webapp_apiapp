03edec0faff174c8178a042f4cc1e3b7200102af
Nam G VU <namgivu@gmail.com>
2022-12-29 22:34:36 +0700

    when sh shell set as /app

--- flask_apiapp/script/start.sh
+++ flask_apiapp/script/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SH=$(cd `dirname $BASH_SOURCE` && pwd)
-AH=$(cd "$SH/.." && pwd)
+
+AH=$(cd "$SH/.." && pwd) ; [[ -z $BASH_SOURCE ]] && AH='/app'
+#                        ;       $BASH_SOURCE not avail. in sh shell of python:3.8 container, only avail. in bash shell, so we hardcode it as /app here
 
 cd $AH
     PYTHONPATH=$AH \
