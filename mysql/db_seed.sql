0e171b040dbe3a9c89163191908cf5d1a846a278
Nam G VU <namgivu@gmail.com>
2023-04-27 18:08:03 +0700

    v2 w/ mysql_c auto dbseed

---
+++ mysql/db_seed.sql
@@ -0,0 +1,16 @@
+DROP TABLE if exists BinhLuan;
+
+CREATE TABLE BinhLuan (
+    `id` int AUTO_INCREMENT,
+
+    name   text,
+    fee    text,
+    `desc` text,
+
+    PRIMARY KEY(id)
+);
+
+INSERT INTO BinhLuan (`name`, `fee`, `desc`)
+              VALUES ('Docker Container',  '2,500,000₫', 'Khóa học trang bị nền tảng kiến thức về container và triển khai ứng dụng theo kiến trúc microservice'),
+                     ('Python for DevOps', '5,500,000₫', 'Tổng quan: Python hiện đang là 1 trong những ngôn ngữ lập trình phổ biến nhất cho người mới bắt đầu với CNTT nhờ…')
+;
--- mysql/start.sh
+++ mysql/start.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 SH=$(cd `dirname $BASH_SOURCE` && pwd)
 
-c=mysql_cau3_c ; p=20829 v="$SH/v"
+c='mysql__toda20_cau3_c' ; p=20829 ; v='mysql__toda20_cau3_v' ; n='mysql__toda20_cau3_n'
+
+echo;echo;
+docker network create $n
+
+# mysql c
+echo;echo;
 docker rm -f $c ; docker run -d  --name $c -h$c \
     -p "$p:3306" \
     \
@@ -9,15 +15,30 @@ docker rm -f $c ; docker run -d  --name $c -h$c \
     -e MYSQL_DATABASE=db \
     \
     -v "$v:/var/lib/mysql" \
+    -v "$SH/db_seed.sql":/docker-entrypoint-initdb.d/'db_seed.sql'  `# run dbseed script  ref. https://hub.docker.com/_/mysql  > Initializing a fresh instance` \
+    \
+    --network $n \
     mysql ; [ $? != 0 ] && exit
 
+    #region checkport
     echo ; echo '--- checkport'
-        set -x  # exec print ON
-            echo ; nmap -p$p localhost    | grep -E "$p.+open" --color=always ; checkport_nmap=$?
-            echo ; nc -z     localhost $p ; checkport_nc=$?
-        set +x  # exec print OFF
+        echo ; nmap -p$p localhost    | grep -E "$p.+open" --color=always ; checkport_nmap=$?
+        echo ; nc -z     localhost $p ; checkport_nc=$?
 
-        echo
         echo "checkport_nmap=$checkport_nmap"
         echo "checkport_nc=$checkport_nc"
         echo '--- checkport END'
+    #endregion checkport
+
+
+# GUI db client @ adminer
+echo;echo;
+docker rm -f "adminer_$c"; docker run -d    \
+    -p8080:8080  \
+    --network $n \
+    --name "adminer_$c" \
+    adminer
+
+
+echo;echo;
+docker ps | grep -E "$c|adminer_$c"
