eac354fa2e64285161015cfdf64598688c8eca3d
Nam G VU <namgivu@gmail.com>
2023-04-28 15:43:45 +0700

    import pymysql; pymysql.install_as_MySQLdb()  # fix err > ModuleNotFoundError: No module named 'MySQLdb'

--- flask_apiapp/script/data/add.py
+++ flask_apiapp/script/data/add.py
@@ -1,7 +1,9 @@
 from service.envvar import ensure_envvar_DB_xx_defined
 #
-from service.db.model_class import Base, Tel4vnCourse
+import pymysql; pymysql.install_as_MySQLdb()  # fix err > ModuleNotFoundError: No module named 'MySQLdb'  ref. https://stackoverflow.com/a/71730129/248616
+#
 from service.db.mysql import scoped_engine, dbsession
+from service.db.model_class import Base, Tel4vnCourse
 
 
 ensure_envvar_DB_xx_defined()
