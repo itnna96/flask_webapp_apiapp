30964ac98ed18ab8224ed4d1386295a4e6fd4da5
Nam G VU <namgivu@gmail.com>
2023-04-28 08:59:14 +0700

    review db @ tablename

--- mysql/db_seed.sql
+++ mysql/db_seed.sql
@@ -1,6 +1,6 @@
 DROP TABLE if exists BinhLuan;
 
-CREATE TABLE BinhLuan (
+CREATE TABLE tel4vn_courses (
     `id` int AUTO_INCREMENT,
 
     name   text,
@@ -10,7 +10,7 @@ CREATE TABLE BinhLuan (
     PRIMARY KEY(id)
 );
 
-INSERT INTO BinhLuan (`name`, `fee`, `desc`)
-              VALUES ('Docker Container',  '2,500,000₫', 'Khóa học trang bị nền tảng kiến thức về container và triển khai ứng dụng theo kiến trúc microservice'),
-                     ('Python for DevOps', '5,500,000₫', 'Tổng quan: Python hiện đang là 1 trong những ngôn ngữ lập trình phổ biến nhất cho người mới bắt đầu với CNTT nhờ…')
+INSERT INTO tel4vn_courses (`name`, `fee`, `desc`)
+                    VALUES ('Docker Container',  '2,500,000₫', 'Khóa học trang bị nền tảng kiến thức về container và triển khai ứng dụng theo kiến trúc microservice'),
+                           ('Python for DevOps', '5,500,000₫', 'Tổng quan: Python hiện đang là 1 trong những ngôn ngữ lập trình phổ biến nhất cho người mới bắt đầu với CNTT nhờ…')
 ;
