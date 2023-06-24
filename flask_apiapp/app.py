e07edaff0cbd16b73dedc4abdd3da388abb94643
Nam G VU <namgivu@gmail.com>
2023-06-24 22:22:06 +0700

    rename /tel4vncourse

--- flask_apiapp/app.py
+++ flask_apiapp/app.py
@@ -18,31 +18,31 @@ def index():
     return GET_tel4vn_course()
 
 
-#region /tel4vn_course
-def GET_tel4vn_course():
+#region /tel4vncourse
+def GET_tel4vncourse():
     r_all = dbsession.query(Tel4vnCourse).all()
     return jsonify(r_all)
 
-@app.route('/tel4vn_course', methods=['GET'])
-def tel4vn_course():
-    return GET_tel4vn_course()
+@app.route('/tel4vncourse', methods=['GET'])
+def tel4vncourse():
+    return GET_tel4vncourse()
 
 
 #region mock
-@app.route('/mock/tel4vn_course', methods=['GET'])
-def mock_tel4vn_course():
-    return GET_mock_tel4vn_course()
+@app.route('/mock/tel4vncourse', methods=['GET'])
+def mock_tel4vncourse():
+    return GET_mock_tel4vncourse()
 
 mock_course_l = [
     {'name': 'Docker Container',  'fee': '2,500,000₫', 'desc': 'Khóa học trang bị nền tảng kiến thức về container và triển khai ứng dụng theo kiến trúc microservice', },
     {'name': 'Python for DevOps', 'fee': '5,500,000₫', 'desc': 'Tổng quan: Python hiện đang là 1 trong những ngôn ngữ lập trình phổ biến nhất cho người mới bắt đầu với CNTT nhờ…', },
 ]
 
-def GET_mock_tel4vn_course():
+def GET_mock_tel4vncourse():
     return jsonify(mock_course_l)
 #endregion mock
 
-#endregion /tel4vn_course
+#endregion /tel4vncourse
 
 
 if __name__ == '__main__':
